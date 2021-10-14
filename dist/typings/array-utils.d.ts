f407924210ce69a358115dec93d8d24fa9b9024a
Vykash <aaron975@hotmail.com>
2021-10-14 16:54:39 -0700

    better type safety with copy. fix logic bug with resizeBuffer. added swap and swapNPop functions

--- dist/typings/array-utils.d.ts
+++ dist/typings/array-utils.d.ts
@@ -37,14 +37,29 @@ export interface IIntegerBufferArrayConstructor<T extends TIntegerBufferArrays> 
 }
 export declare type TBufferArrays = Float64Array | Float32Array | Uint32Array | Int32Array | Uint16Array | Int16Array | Uint8Array | Uint8ClampedArray | Int8Array;
 export declare type TFloatBufferArrays = Float64Array | Float32Array;
+export declare type TInferIndexableGeneric<T> = T extends TIndexable<infer A> ? A : never;
 export declare type TIntegerBufferArrays = Uint32Array | Uint32Array | Int32Array | Uint16Array | Int16Array | Uint8Array | Uint8ClampedArray | Int8Array;
 /**
  * copies source to target in range of [from, to).
- * does not preform any error checking.
+ * does not validate indices.
  */
-export declare const copy: <T extends TIndexable<any>, U extends TIndexable<any>>(target: T, source: U, from: number, to: number) => T;
+export declare const copy: <T extends TIndexable<any>, U extends TIndexable<TInferIndexableGeneric<T>>>(target: T, source: U, from: number, to: number) => T;
 /**
  * returns a new buffer array with length of newSize
  */
 export declare const resizeBuffer: <T extends TBufferArrays>(buffer: T, newSize: number) => T;
+/**
+ * does not validate indices.
+ * @returns array with values at a and b swapped
+ */
+export declare const swap: <T extends TIndexable<any>>(array: T, a: number, b: number) => T;
+/**
+ * a faster remove method than `splice` for UNORDERED arrays. the value at indexToRemove is replaced
+ * with the result of array.pop(). does not validate indices
+ * @returns the removed value or undefined if array is empty (same as default pop behavior)
+ */
+export declare const swapNPop: <T extends TIndexable<any> & {
+    length: number;
+    pop(): TInferIndexableGeneric<T> | undefined;
+}>(array: T, indexToRemove: number) => TInferIndexableGeneric<T> | undefined;
 export {};
