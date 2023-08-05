891f3b20c1c87ba2f39d9a75afeac343b967ed54
Aaron McDonald <Aaron975@hotmail.com>
2023-08-04 18:41:31 -0700

    comment adjustments, doc adjustments

--- dist/array-utils.es.js
+++ dist/array-utils.es.js
@@ -1,16 +1,9 @@
-/**
- * copies source to target in range of [from, to).
- * does not validate indices.
- */
 const copy = (target, source, from, to) => {
     for (; from < to; ++from) {
         target[from] = source[from];
     }
     return target;
 };
-/**
- * returns a new buffer array with length of newSize
- */
 const resizeBuffer = (buffer, newSize) => {
     const viewCnstr = buffer['constructor'];
     const newBuff = new ArrayBuffer(newSize * buffer.BYTES_PER_ELEMENT);
@@ -18,10 +11,6 @@ const resizeBuffer = (buffer, newSize) => {
     copy(newView, buffer, 0, Math.min(buffer.length, newSize));
     return newView;
 };
-/**
- * does not validate indices.
- * @returns array with values at a and b swapped
- */
 const swap = (array, a, b) => {
     const temp = array[a];
     array[a] = array[b];
@@ -30,8 +19,9 @@ const swap = (array, a, b) => {
 };
 /**
  * a faster remove method than `splice` for UNORDERED arrays. the value at indexToRemove is replaced
- * with the result of array.pop(). does not validate indices
- * @returns the removed value or undefined if array is empty (same as default pop behavior)
+ * with the result of array.pop(). does not validate indices. if indexToRemove is >= array.length then
+ * the pop() result is returned without any swapping.
+ * @returns standard pop results
  */
 const swapNPop = (array, indexToRemove) => {
     const swapVal = array.pop();
--- dist/array-utils.js
+++ dist/array-utils.js
@@ -4,19 +4,12 @@
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.AU = {}));
 })(this, (function (exports) { 'use strict';
 
-    /**
-     * copies source to target in range of [from, to).
-     * does not validate indices.
-     */
     const copy = (target, source, from, to) => {
         for (; from < to; ++from) {
             target[from] = source[from];
         }
         return target;
     };
-    /**
-     * returns a new buffer array with length of newSize
-     */
     const resizeBuffer = (buffer, newSize) => {
         const viewCnstr = buffer['constructor'];
         const newBuff = new ArrayBuffer(newSize * buffer.BYTES_PER_ELEMENT);
@@ -24,10 +17,6 @@
         copy(newView, buffer, 0, Math.min(buffer.length, newSize));
         return newView;
     };
-    /**
-     * does not validate indices.
-     * @returns array with values at a and b swapped
-     */
     const swap = (array, a, b) => {
         const temp = array[a];
         array[a] = array[b];
@@ -36,8 +25,9 @@
     };
     /**
      * a faster remove method than `splice` for UNORDERED arrays. the value at indexToRemove is replaced
-     * with the result of array.pop(). does not validate indices
-     * @returns the removed value or undefined if array is empty (same as default pop behavior)
+     * with the result of array.pop(). does not validate indices. if indexToRemove is >= array.length then
+     * the pop() result is returned without any swapping.
+     * @returns standard pop results
      */
     const swapNPop = (array, indexToRemove) => {
         const swapVal = array.pop();
